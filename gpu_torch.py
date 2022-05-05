dd3d5cfcb750031d55287fc3dda60a6567ef084d
Nicolas Tirel <ntirel@univ-pau.fr>
2022-05-05 17:19:21 +0200

    update experiment and plot

--- gpu_torch.py
+++ gpu_torch.py
@@ -1,55 +1,52 @@
 import torch
-import subprocess
-import numpy as np
-import matplotlib.pyplot as plt
 from AIPowerMeter.deep_learning_power_measure.power_measure import experiment, parsers
-import os
 import time
 
-log_file = 'wm_log'
+# How much matrices multiplication you want
+iters = int(1000000)
 
+# Dense result with a varying size
 def dense_varying(sizes = [2**i for i in range(2,13)]):
     for i,size in enumerate(sizes):
-        iters = int(1000000)
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         densemat = torch.randn(size,size).to(device)
         multmat = torch.randn(size,size).to(device)
 
         dir_name = f"logs/dense_varying/{size}"
-        #driver = parsers.JsonParser(dir_name)
-        #exp = experiment.Experiment(driver)
+        driver = parsers.JsonParser(dir_name)
+        exp = experiment.Experiment(driver)
 
-        #p, q = exp.measure_yourself(period=2) # measure every 2 seconds
+        p, q = exp.measure_yourself(period=2) # measure every 2 seconds
         start_xp = time.time()
         for _ in range(iters):
             torch.mm(densemat,multmat)
         print(round(time.time()-start_xp,3))
         
-        #q.put(experiment.STOP_MESSAGE)
+        q.put(experiment.STOP_MESSAGE)
 
-
+# Sparse result with a varying size n and a varying density 1/n
 def sparse_varying(sizes = [2**i for i in range(2,13)]):
     for i,size in enumerate(sizes):
-        iters = int(1000000)
-        #print('Multiplying matrices of size = {0}x{1}'.format(size,size))
+        print('Multiplying matrices of size = {0}x{1}'.format(size,size))
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         spmat = torch.sparse_coo_tensor(indices = torch.tensor([torch.arange(size).tolist(),torch.arange(size).tolist()]), values = torch.randn(size), size=[size,size], dtype=torch.float32, device=device)
         multmat = torch.randn(size,size).to(device)
 
+        # Track energy and time consumption and store the result in a file 
         dir_name = f"logs/sparse_varying/{size}"
         driver = parsers.JsonParser(dir_name)
-        #exp = experiment.Experiment(driver)
+        exp = experiment.Experiment(driver)
 
-        #p, q = exp.measure_yourself(period=2) # measure every 2 seconds
+        p, q = exp.measure_yourself(period=2) # measure every 2 seconds
         start_xp = time.time()
         for _ in range(iters):
             torch.sparse.mm(spmat,multmat)
         print(round(time.time()-start_xp,3))
-        #q.put(experiment.STOP_MESSAGE)
+        q.put(experiment.STOP_MESSAGE)
 
+# Sparse result with a varying size n and a fixed density of 1/100
 def sparse_fixed(sizes = [2**i for i in range(2,13)]):
     for i,size in enumerate(sizes):
-        iters = int(1000000)
         print('Multiplying matrices of size = {0}x{1}'.format(size,size))
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         nnz = size*size//100
@@ -61,10 +58,13 @@ def sparse_fixed(sizes = [2**i for i in range(2,13)]):
         exp = experiment.Experiment(driver)
 
         p, q = exp.measure_yourself(period=2) # measure every 2 seconds
+        start_xp = time.time()
         for _ in range(iters):
             torch.sparse.mm(spmat,multmat)
+        print(round(time.time()-start_xp,3))
         q.put(experiment.STOP_MESSAGE)
 
+# Print the consumption results for all the size at the end of the experiment
 def print_results(sizes = [2**i for i in range(2,13)]):
     for i,size in enumerate(sizes):
         filename = f"input_{size}"
@@ -79,7 +79,7 @@ def print_results(sizes = [2**i for i in range(2,13)]):
         exp_result = experiment.ExpResults(driver)
         exp_result.print()
 
-        print(f"Power Consumption from sparse matrices multiplication of size {size}")
+        print(f"Power Consumption from sparse matrices multiplication of size {size} with fixed density of 1/100")
         driver = parsers.JsonParser(f"logs/sparse_fixed/{size}")
         exp_result = experiment.ExpResults(driver)
         exp_result.print()
@@ -87,14 +87,12 @@ def print_results(sizes = [2**i for i in range(2,13)]):
 
 # Main execution
 if __name__ == '__main__':
-    sizes = [2**i for i in range(2,13)]
-    #subprocess.call(['ssh', 'ntirel@10.0.12.102', '/home/ntirel/Documents/wattmetre-read', '--tty=/dev/ttyUSB0', '--nb=6', '>', log_file, '2>&1', '&', 'echo', '$!', '>', 'wm_pid'])
-    #dense_varying(sizes)
-    #print("------------------------")
+
+    sizes = [2**i for i in range(9,10)]
+    # Execute the multiplication and save the results
+    
+    dense_varying(sizes)
     sparse_varying(sizes)
-    #sparse_fixed(sizes)
+    sparse_fixed(sizes)
 
-    #print_results(sizes)
-
-    #subprocess.call(['ssh', 'ntirel@10.0.12.102', 'kill', '-10', '`cat', 'wm_pid`'])
-    #subprocess.call(['scp', f'ntirel@10.0.12.102:{log_file}', f'/data/ntirel/sparsity/{dir_name}{log_file}'])
+    print_results(sizes)
--- plot_results.py
+++ plot_results.py
@@ -1,12 +1,12 @@
 from AIPowerMeter.deep_learning_power_measure.power_measure import experiment, parsers
 import pandas as pd
-from datetime import datetime, timedelta
-import numpy as np
 import matplotlib.pyplot as plt
 
 sizes = [2**i for i in range(2,13)]
 dense = []
 sparse = []
+dense_time = [5.188,4.704,4.855,4.858,4.882,6.077,7.457,12.932,78.153,600.904,4010.097]
+sparse_time = [128.377,127.922,129.095,129.968,131.497,132.283,138.695,162.529,226.504,409.913,1139.658]
 fixed = []
 for i,size in enumerate(sizes):
     driver = parsers.JsonParser(f"logs/dense_varying/{size}")
@@ -23,7 +23,7 @@ for i,size in enumerate(sizes):
     fixed.append(rel_nvidia_power)
 
 
-data = {"size" : sizes, "dense" : dense, "sparse" : sparse, "fixed" : fixed}
+data = {"sizes" : sizes, "dense" : dense, "dense_time" : dense_time, "sparsee" : sparse, "sparse_time" : sparse_time, "fixed" : fixed}
 df_results = pd.DataFrame(data)
 print(df_results)
 
@@ -45,44 +45,78 @@ def print_results(sizes = [2**i for i in range(2,13)]):
         driver = parsers.JsonParser(f"logs/sparse_fixed/{size}")
         exp_result = experiment.ExpResults(driver)
         exp_result.print()
-
-# scatter plot
-"""
-df_results.plot(kind = 'line',
-        x = 'size',
-        y = 'dense',
-        logx = True,
-        color = 'green')
-
-df_results.plot(kind = 'line',
-        x = 'size',
-        logx = True,
-        y = 'sparse',
-        color = 'blue')
-
-df_results.plot(kind = 'line',
-        x = 'size',
-        logx = True,
-        y = 'fixed',
-        color = 'black')"""
   
-df_results[["size", "dense", "fixed"]].plot(x="size")
+#define colors to use
+col11 = 'lightcoral'
+col12 = 'cornflowerblue'
+col21 = 'orangered'
+col22 = 'navy'
+
+#define subplots
+fig,ax = plt.subplots()
+#add first line to plot
+ax.plot(df_results.sizes, df_results.dense, color=col21, marker='o', linewidth=1)
+ax.plot(df_results.sizes, df_results["sparsee"], color=col11, marker='o', linewidth=1)
+
+#add x-axis label
+ax.set_xlabel('Sizes', fontsize=14)
+
+#add y-axis label
+ax.set_ylabel('Consumption (joules)', color="blue", fontsize=16)
+
+#define second y-axis that shares x-axis with current plot
+ax2 = ax.twinx()
+
+#add second line to plot
+ax2.plot(df_results.sizes, df_results.dense_time, color=col22, marker='o', linewidth=1)
+ax2.plot(df_results.sizes, df_results.sparse_time, color=col12, marker='o', linewidth=1)
+
+#add second y-axis label
+ax2.set_ylabel('Time (seconds)', color="red", fontsize=16)
+
 plt.semilogx(base=2)
+
 # set the title
 plt.title('Dense vs Sparse')
-plt.xlabel('Size (n) of nxn matrix', fontsize=16)
-plt.ylabel('Consumption (joules)', fontsize=16)
 plt.grid()
   
-# show the plot
+# Save the plot
 #plt.show()
-plt.savefig("fig_1.png")
-#print(df_results[df_results["size"] <= 4096])
-df_results[df_results["size"] < 512][["size", "dense", "fixed"]].plot(x="size")
+plt.savefig("comparison.png", bbox_inches='tight')
+
+
+
+# Another graph but zoomed on a specific point
+
+df_results = df_results[df_results["sizes"] >= 512]
+
+#define subplots
+fig,ax = plt.subplots()
+#add first line to plot
+ax.plot(df_results.sizes, df_results.dense, color=col21, marker='o', linewidth=1)
+ax.plot(df_results.sizes, df_results.sparsee, color=col11, marker='o', linewidth=1)
+
+#add x-axis label
+ax.set_xlabel('Sizes', fontsize=14)
+
+#add y-axis label
+ax.set_ylabel('Consumption (joules)', color="blue", fontsize=16)
+
+#define second y-axis that shares x-axis with current plot
+ax2 = ax.twinx()
+
+#add second line to plot
+ax2.plot(df_results.sizes, df_results.dense_time, color=col22, marker='o', linewidth=1)
+ax2.plot(df_results.sizes, df_results.sparse_time, color=col12, marker='o', linewidth=1)
+
+#add second y-axis label
+ax2.set_ylabel('Time (seconds)', color="red", fontsize=16)
+
 plt.semilogx(base=2)
+
 # set the title
 plt.title('Dense vs Sparse')
-plt.xlabel('Size (n) of nxn matrix', fontsize=16)
-plt.ylabel('Consumption (joules)', fontsize=16)
 plt.grid()
-plt.savefig("fig_2.png")
+  
+# Save the plot
+plt.savefig("comparisonZoom.png", bbox_inches='tight')
